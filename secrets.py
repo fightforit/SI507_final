d9adbf2b8b56a9d44dfc585d9b7ea2f46943b0c0
Cheng-Hsun Lu <lchsun@z02.eecs.umich.edu>
2022-04-11 21:47:55 -0400

    remove secrets for savety

--- secrets.py
+++
@@ -1,6 +0,0 @@
-# The private key for Yelp API
-YELP_Client_ID = "nVPFt-oAHvvR5xL6YO5UbA"
-YELP_API_KEY = "9vWArEABs78A5LzvaiAyFXI6toukF5k9blguadmeGQZGmt9lXNyTl_SnAok4TJd62W_xPkH2SXuhoqpF--1lELdhdjP5GeKaY-oXldfrwE6fM09juhmirVQG3yo6YnYx"
-GOOGLE_API_KEY = "AIzaSyCb7wp2x02CQIEh9S3ivOUBLLyPFqpjW7c"
-
-
